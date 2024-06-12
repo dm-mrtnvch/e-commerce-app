0cae4c363236d1da22935e66ad0e036c3f3cbc7c
Abdikhaniyev <alisher147741@gmail.com>
2024-06-13 00:57:16 +0600

    feat: add cart actions

---
+++ src/redux/services/card.ts
@@ -0,0 +1,49 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Cart, CartDraft, CartUpdateAction } from '../../types/cart.ts';
+import { Pagination } from '../../types/common.ts';
+import { RootState } from '../store';
+
+const API_BASE_URL = import.meta.env.VITE_HOST;
+const PROJECT_KEY = import.meta.env.VITE_PROJECT_KEY;
+
+export const cardApi = createApi({
+  reducerPath: 'cardApi',
+  tagTypes: ['Cart'],
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${API_BASE_URL}/${PROJECT_KEY}`,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.clientCredentials?.access_token;
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      headers.set('Content-Type', 'application/json');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getUserCart: builder.query<Pagination<Cart>, void>({
+      query: () => ({
+        url: '/me/carts',
+        method: 'GET',
+        providesTags: ['Cart'],
+      }),
+    }),
+    createCart: builder.mutation<Cart, CartDraft>({
+      query: (cart) => ({
+        url: '/me/carts',
+        method: 'POST',
+        body: cart,
+      }),
+    }),
+    updateCart: builder.mutation<Cart, { id: string; version: number; actions: CartUpdateAction[] }>({
+      query: (cart) => ({
+        url: `/me/carts/${cart.id}`,
+        method: 'POST',
+        body: cart,
+      }),
+      invalidatesTags: ['Cart'],
+    }),
+  }),
+});
+
+export const { useGetUserCartQuery, useLazyGetUserCartQuery, useCreateCartMutation, useUpdateCartMutation } = cardApi;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,20 +1,22 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/authSlice';
 import { authApi } from './services/auth';
+import { cardApi } from './services/card.ts';
+import { categoryApi } from './services/category.ts';
+import { meApi } from './services/me.ts';
 import { productProjectionsApi } from './services/product-projections';
 import { productTypesApi } from './services/product-types';
 import { productsApi } from './services/products';
-import { categoryApi } from './services/category.ts';
-import { meApi } from './services/me.ts';
 
 const rootReducer = combineReducers({
-  auth: authReducer,
   [authApi.reducerPath]: authApi.reducer,
+  [cardApi.reducerPath]: cardApi.reducer,
+  [categoryApi.reducerPath]: categoryApi.reducer,
+  [meApi.reducerPath]: meApi.reducer,
   [productProjectionsApi.reducerPath]: productProjectionsApi.reducer,
   [productsApi.reducerPath]: productsApi.reducer,
   [productTypesApi.reducerPath]: productTypesApi.reducer,
-  [categoryApi.reducerPath]: categoryApi.reducer,
-  [meApi.reducerPath]: meApi.reducer,
+  auth: authReducer,
 });
 
 export const setupStore = () => {
@@ -23,11 +25,12 @@ export const setupStore = () => {
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(
         authApi.middleware,
+        cardApi.middleware,
+        categoryApi.middleware,
+        meApi.middleware,
         productProjectionsApi.middleware,
         productsApi.middleware,
         productTypesApi.middleware,
-        categoryApi.middleware,
-        meApi.middleware,
       ),
   });
 };
